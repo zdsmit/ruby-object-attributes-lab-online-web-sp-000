8fc07fcfd674bc95ad2c55d54cfed939ac190a12
Zach  Smith <zdsmith001@gmail.com>
2019-08-13 22:48:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,12 @@ class Dog
   def name=(dogs_name)
     @name = dogs_name
   end
-
   def name
     @name
   end
-
   def breed=(dogs_breed)
     @breed = dogs_breed
   end
-
   def breed
     @breed
   end
